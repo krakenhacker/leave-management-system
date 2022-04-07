020abd6a508e7153190d5bc6d3a41dca2b6733c4
krakenhacker <george.sot@windowslive.com>
2022-04-07 18:40:32 +0300

    #1 DONE: Login page DONE: Employee can sign into portal and logout. Employee can logout too. DONE: Display list of past submissions
    closes #1

--- index.php
+++ index.php
@@ -28,7 +28,6 @@ if(isset($_POST['signin'])) {
     <meta charset="utf-8">
     <title>Leave Portal</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
