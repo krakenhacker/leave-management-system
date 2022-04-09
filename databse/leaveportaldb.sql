cea7be1342c753ab51d7c1d52be3d36c5459eae8
krakenhacker <george.sot@windowslive.com>
2022-04-09 12:13:25 +0300

    updating sql script file, add the ER diagram of the database

--- databse/leaveportaldb.sql
+++ databse/leaveportaldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Εξυπηρετητής: 127.0.0.1
--- Χρόνος δημιουργίας: 08 Απρ 2022 στις 16:47:53
+-- Χρόνος δημιουργίας: 09 Απρ 2022 στις 10:54:51
 -- Έκδοση διακομιστή: 10.4.24-MariaDB
 -- Έκδοση PHP: 7.4.28
 
@@ -41,8 +41,7 @@ CREATE TABLE `employees` (
 --
 
 INSERT INTO `employees` (`id`, `firstname`, `lastname`, `email`, `password`, `roleid`) VALUES
-(1, 'Georgios', 'Garoufalis', 'geogar@gmail.com', '098f6bcd4621d373cade4e832627b4f6', 2),
-(3, 'marinos', 'charitos', 'charitos@gmail.com', '8ccb35431fea39f0cb950790af1c55fa', 1);
+(19, 'GEORGIOS', 'GAROUFALIS', 'george.sot@windowslive.com', '21232f297a57a5a743894a0e4a801fc3', 2);
 
 -- --------------------------------------------------------
 
@@ -51,18 +50,17 @@ INSERT INTO `employees` (`id`, `firstname`, `lastname`, `email`, `password`, `ro
 --
 
 CREATE TABLE `emp_role` (
-  `id` int(10) NOT NULL,
-  `role` varchar(50) NOT NULL,
-  `employeeid` int(10) NOT NULL
+  `id` int(11) NOT NULL,
+  `role` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Άδειασμα δεδομένων του πίνακα `emp_role`
 --
 
-INSERT INTO `emp_role` (`id`, `role`, `employeeid`) VALUES
-(1, 'Employee', 3),
-(2, 'Admin', 1);
+INSERT INTO `emp_role` (`id`, `role`) VALUES
+(1, 'Employee'),
+(2, 'Admin');
 
 -- --------------------------------------------------------
 
@@ -102,17 +100,6 @@ CREATE TABLE `submissions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Άδειασμα δεδομένων του πίνακα `submissions`
---
-
-INSERT INTO `submissions` (`id`, `date_submitted`, `vacstart`, `vacend`, `totaldays`, `statusid`, `employeeid`, `reason`) VALUES
-(1, '2022-04-07', '2022-04-22', '2022-04-25', 3, 1, 1, 'summer vacations'),
-(5, '2022-04-08', '2022-04-08', '2022-04-23', 15, 1, 1, 'test'),
-(15, '2022-04-08', '2022-04-12', '2022-04-15', 3, 1, 1, '3'),
-(16, '2022-04-08', '2022-04-10', '2022-04-13', 3, 1, 1, 'i got hurt my leg'),
-(18, '2022-04-08', '2022-04-15', '2022-05-20', 35, 1, 1, 'testlong\r\n');
-
---
 -- Ευρετήρια για άχρηστους πίνακες
 --
 
@@ -120,14 +107,15 @@ INSERT INTO `submissions` (`id`, `date_submitted`, `vacstart`, `vacend`, `totald
 -- Ευρετήρια για πίνακα `employees`
 --
 ALTER TABLE `employees`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`) USING BTREE,
+  ADD KEY `roleid` (`roleid`);
 
 --
 -- Ευρετήρια για πίνακα `emp_role`
 --
 ALTER TABLE `emp_role`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `employeeid` (`employeeid`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Ευρετήρια για πίνακα `stsubmission`
@@ -151,23 +139,23 @@ ALTER TABLE `submissions`
 -- AUTO_INCREMENT για πίνακα `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT για πίνακα `submissions`
 --
 ALTER TABLE `submissions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Περιορισμοί για άχρηστους πίνακες
 --
 
 --
--- Περιορισμοί για πίνακα `emp_role`
+-- Περιορισμοί για πίνακα `employees`
 --
-ALTER TABLE `emp_role`
-  ADD CONSTRAINT `emp_role_ibfk_1` FOREIGN KEY (`employeeid`) REFERENCES `employees` (`id`);
+ALTER TABLE `employees`
+  ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`roleid`) REFERENCES `emp_role` (`id`);
 
 --
 -- Περιορισμοί για πίνακα `submissions`
