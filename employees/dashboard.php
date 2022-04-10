0dff3c07bb12f5759e772cb8f933b39b1a6fc4dd
krakenhacker <george.sot@windowslive.com>
2022-04-11 02:54:14 +0300

    employee dashboard list of past application status design upgrade

--- employees/dashboard.php
+++ employees/dashboard.php
@@ -9,6 +9,7 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/3cbcc35af1.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
@@ -30,7 +31,17 @@ if(isset($_SESSION["empname"])) {
 //        echo "<div class='col-md-2'></div>";
         echo "<div class='container col-md-8'><table class='table table-striped table-hover'><tr><th>Date submitted</th><th>Vacation Start</th><th>Vacation End</th><th>Days requested</th><th>Status</th></tr>";
         while($row = $result->fetch_assoc()) {
-            echo "<tr><td>".$row["date_submitted"]."</td><td>".$row["vacstart"]."</td><td>".$row["vacend"]."</td><td>".$row["totaldays"]."</td><td>".$row["status"]."</td></tr>";
+            if($row["statusid"] == 2){
+                $icon="<i class='far fa-calendar-check' style='color: limegreen'></i>";
+                $color="limegreen";
+            }elseif ($row["statusid"] == 3){
+                $icon="<i class='far fa-calendar-times' style='color: red'></i>";
+                $color="red";
+            }else{
+                $icon="<i class='fas fa-spinner fa-pulse'></i>";
+                $color="black";
+            }
+            echo "<tr><td>".$row["date_submitted"]."</td><td>".$row["vacstart"]."</td><td>".$row["vacend"]."</td><td>".$row["totaldays"]."</td><td style='color: ".$color.";'>". $icon."  ".$row["status"]."</td></tr>";
         }
         echo "</table></div></div>";
 //        echo "<div class='col-md-2'></div>";
